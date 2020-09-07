a6d931ff40a9ccc5f28287db94d4fe318a2a92e0
Satyarth Misra <misrasatyarth@outlook.com>
2020-09-07 23:08:35 +0530

    something

--- IdentityAuthenticationAspNetCore/Startup.cs
+++ IdentityAuthenticationAspNetCore/Startup.cs
@@ -30,12 +30,7 @@ namespace IdentityAuthenticationAspNetCore
             {
                 options.UseSqlServer(Configuration.GetConnectionString("RestaurantIdentityConnection"));
             });
-            services.AddControllersWithViews(options=>
-            {
-                options.Filters.Add(new AuthorizeFilter());
-            });
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
